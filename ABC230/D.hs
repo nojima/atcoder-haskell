914807fafcc826a8a20325c0d26b37a1cfbe834c
Yusuke Nojima <nojima@ynojima.com>
2021-12-17 23:33:56 +0900

    use vector

--- ABC230/D.hs
+++ ABC230/D.hs
@@ -5,31 +5,34 @@
 import qualified Data.Attoparsec.ByteString.Char8 as Parsec
 import qualified Data.ByteString.Char8 as ByteString
 import           Data.ByteString.Char8 (ByteString)
+import           Data.Ord (comparing)
 import           Data.List (sortOn, foldl')
-
-data Wall = Wall Int Int
-  deriving (Eq, Ord)
+import qualified Data.Vector.Unboxed as Vector
+import           Data.Vector.Unboxed (Vector, MVector)
+import qualified Data.Vector.Algorithms.Intro as Vector
 
 main :: IO ()
 main = do
   answer <- parseStdin $
     solve <$> int
           <*> int
-          <*> Parsec.many1 (Wall <$> int <*> int)
+          <*> many1 ((,) <$> int <*> int)
   print answer
 
-solve :: Int -> Int -> [Wall] -> Int
+solve :: Int -> Int -> Vector (Int, Int) -> Int
 solve n d walls =
   let
     wallsSorted =
-      sortOn (\(Wall l r) -> Wall r l) walls
+      Vector.modify
+        (Vector.sortBy (comparing \(l, r) -> (r, l)))
+        walls
 
-    step (!min_alive, !n_punch) (Wall l r)
+    step (!min_alive, !n_punch) (!l, !r)
       | l < min_alive = (min_alive, n_punch)
       | otherwise     = (r + d, n_punch + 1)
 
     (_, !min_punch) =
-      foldl' step (0, 0) wallsSorted
+      Vector.foldl' step (0, 0) wallsSorted
   in
     min_punch
 
@@ -38,6 +41,9 @@ solve n d walls =
 
 int :: Parsec.Parser Int
 int = Parsec.skipSpace *> Parsec.decimal
+
+many1 :: Vector.Unbox a => Parsec.Parser a -> Parsec.Parser (Vector a)
+many1 parser = Vector.fromList <$> Parsec.many1 parser
 
 parseStdin :: Parsec.Parser a -> IO a
 parseStdin parser = do
